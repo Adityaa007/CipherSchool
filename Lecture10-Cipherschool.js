1ffce22a279dce8922bb737752d8f29e97218b30
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-06-18 13:04:06 +0530

    updated

---
+++ Lecture10-Cipherschool.js
@@ -0,0 +1,13 @@
+let person={
+    firstName : "Jim" ,
+    lastName: "Harlpet",
+
+    getFullName:function() {
+        return `The name of the person is ${person.firstName} ${person.lastName}`
+    }
+phoneNumber:{
+    mobile: 3434342
+    landline: f
+}
+
+}
