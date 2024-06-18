0d4382dbf629934c40af7d23977ce1ad3d28f793
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-06-18 18:45:23 +0000

    updated

--- Lecture10-Cipherschool.js
+++ Lecture10-Cipherschool.js
@@ -40,3 +40,56 @@ me.name="Jim"
 me.isstudying=true;
 
 me.printintro();
+
+class vehicle{
+    constructor (name,maker,engine){
+        this.name=name;
+        this.maker=maker;
+        this.engine=engine;
+        }
+        getDetails(){
+            return`The name of vehical is ${this.name}`
+        }
+}
+
+let v1=new vehicle("Creta","Hyundai","2500cc")
+let v2=new vehicle("Q5","Audi","4333cc")
+
+console.log(v1.name);
+console.log(v1.maker);
+console.log(v1.getDetails());
+console.log(v2.name);
+console.log(v2.maker);
+console.log(v2.getDetails());
+
+
+
+class prson {
+    constructor(firstName,lastName){
+        this.firstName=firstName
+        this.lastName=lastName
+    }
+
+    getDetails=function(){
+        return `First name is: ${this.firstName} and lastname is: ${this.lastName}`
+    }
+ }  
+   class Student extends prson{
+
+        constructor(firstName, lastName, rollno){
+            super(firstName,lastName)
+            this.rollno=rollno
+        }
+
+        getDetails=function(){
+            return `the roll no is ${this.rollno }`
+        }
+
+    }
+
+    let person3=new prson("Dwight","Micheal")
+    console.log(person3.firstName);
+    console.log(person3.getDetails());
+
+    let Student1=new Student("Ryan","Ghosling",24)
+    console.log(Student1.getDetails());
