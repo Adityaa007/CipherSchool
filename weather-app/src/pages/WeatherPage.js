b54b40de34aaea99dc09d345a94cddad2fc5e48f
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-07-13 10:32:16 +0000

    updated

--- weather-app/src/pages/WeatherPage.js
+++ weather-app/src/pages/WeatherPage.js
@@ -1,44 +1,49 @@
-import { useEffect,useState } from "react";
+import {useEffect, useState} from "react"
 import WeatherRow from "../components/WeatherRow";
 import WeatherSummary from "../components/WeatherSummary";
-import getWeather from "../API/WeatherAPI";
-
-const fetchCoordinates=(callback)=>{
-    navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude}})=>
-    {
-       callback(latitude,longitude)
-    },
-       (err)=> console.error(err)
+import getWeather from "../api/weatherApi";
+const fetchCoordinates = (callback) => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+        callback(latitude,longitude);
+        // console.log(latitude, longitude);
+    }, (err) => {
+        console.error(err);
+    }
     );
 };
 
-const WeatherPage=()=>{
-    const [todayWeather,setTodayWeather]=useState({});
-    const [WeekWeather,setWeekWeather]=useState([]);
+
+
+const WeatherPage = () => {
+
+    const [todayWeather, setTodayWeather] = useState({});
+    const [weekWeather, setWeekWeather] = useState([]);
     const [isCelsius, setIsCelsius] = useState(true);
 
 
-    const isday=todayWeather.isDay ?? true;
-     // callback function , dependancy array 
-    // /? mounting adding to calltack or defined there and unmounting concept removing data from callstack ?? 
 
+    const isDay = todayWeather.isDay ?? true;
+
+    // callback function , dependancy array 
+    // ? mounting adding to calltack or defined there and unmounting concept removing data from callstack ?? 
     useEffect(()=>{
-        fetchCoordinates(async(latitude,longitude)=>{
-          const WeatherInfo= await getWeather({latitude,longitude})
-          console.log(WeatherInfo);
+        fetchCoordinates( async(latitude,longitude)=>{
+            // console.log(latitude,longitude);
+           const WeatherInfo= await getWeather({latitude,longitude})
+        //    console.log(WeatherInfo);
+        convertToStateVariable(WeatherInfo)
         })
     },[])
-    const convertToStateVariable=(tempWeekWeather)=>{
-        let fetchedWeatherInfo=[];
-        for(let i=0;i<tempWeekWeather.daily.time.length;i++){
+
+    const convertToStateVariable= (tempWeekWeather) =>{
+        let fetchedWeatherInfo = [];
+        for(let i=0;i <tempWeekWeather.daily.time.length; i++){
             fetchedWeatherInfo.push({
-                date:new Date(tempWeekWeather.daily.time[i]),
+                date: new Date(tempWeekWeather.daily.time[i]),
                 maxTemperature: tempWeekWeather.daily.temperature_2m_max[i],
                 minTemperature: tempWeekWeather.daily.temperature_2m_min[i],
-                weatherCode:tempWeekWeather.daily.weatherCode[i],
-
-
-            })
+                weatherCode: tempWeekWeather.daily.weathercode[i],
+            });
         }
         setWeekWeather(fetchedWeatherInfo)
 
@@ -55,37 +60,34 @@ const WeatherPage=()=>{
     if(!weekWeather.length){
         return <h3>Loading...</h3>
     }
-    
 
     return (
-        <div className={isday ? "app": "app dark"}>
+        <div className={isDay ? "app" : "app dark"}>
             <h1 className="my-heading">Weather</h1>
-            <button className="ui icon button" onClick={()=>{
-                console.log("Temp Unit Button was clicked");
-            }} style={{float:"right"}}>°F</button>
+
+            <button className="ui icon button" onClick={() => setIsCelsius(!isCelsius)}
+                style={{ float: "right" }}>{isCelsius ? "°F" : "°C"}</button>
+
             <div>
-                <WeatherSummary/>
-                <table className={`ui very basic table ${!isday ? "dark" : ""}`} >
-                    <thead className={`table-custom${!isday ? "dark" : ""}`}>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temperature</th>
-                        <th>Type</th>
-                    </tr>
+                <WeatherSummary currentWeather={todayWeather} isCelsius={isCelsius}/>
+                <table className={`ui very basic table ${!isDay ? "dark" : ""}`} >
+                    <thead className={`table-custom ${!isDay ? "dark" : ""}`}>
+                        <tr>
+                            <th>Date</th>
+                            <th>Temperature</th>
+                            <th>Type</th>
+                        </tr>
                     </thead>
-                    <tbody className="table-custom">
-                        <WeatherRow/>
-                        <WeatherRow/>
-                        <WeatherRow/>
-                        <WeatherRow/>
-                        <WeatherRow/>
-                        <WeatherRow/>
-                        <WeatherRow/>
+
+                    <tbody className=" table-custom">
+                       {weekWeather.map((weather) =>{
+                        return (<WeatherRow weather={weather} isCelsius={isCelsius} key={weather.date}/> );
+                       })}
                     </tbody>
                 </table>
             </div>
         </div>
-    )
-};
+    );
+}
 
 export default WeatherPage;
